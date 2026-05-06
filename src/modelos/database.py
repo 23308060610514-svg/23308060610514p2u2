bf893faa8cbc1aa9d106870440f4e639f4b6dccc
ErikDC <23308060610514@cetis61.edu.mx>
2026-05-06 10:51:42 -0500

    ññññññ

--- src/modelos/database.py
+++ src/modelos/database.py
@@ -5,18 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Database:
-    @staticmethod
-    def get_connection():
-        try:
-            return mysql.connector.connect(
-                host=os.getenv("DB_HOST"),
-                user=os.getenv("DB_USER"),
-                password=os.getenv("DB_PASSWORD"),
-                database=os.getenv("DB_NAME")
-            )
-        
-            if connection.is_connected():
-                return connection 
-            
-            except mysql.connector.Error as err:
+    def get_connection(self):
+        return mysql.connector.connect(
+            host="127.0.0.1", 
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
 
